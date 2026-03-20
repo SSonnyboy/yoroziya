92723b710a46c4bd2e13eaa206676bfc123a6ed6
biabuluo <1802362935@qq.com>
2026-03-21 00:33:56 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://ssonnyboy.github.io/yoroziya/",
+	base: "/yoroziya/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
