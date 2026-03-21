e02690dfb3a6fafb9155b01fcb9e1e1f83e6cb13
biabuluo <1802362935@qq.com>
2026-03-21 10:49:31 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "小宇のyorozuya",
+	title: "小宇の万事屋",
 	subtitle: "Yorozuya",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
